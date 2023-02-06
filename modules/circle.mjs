b57922147f924db97826bf22078c0cfb630dea2a
Ederson Macedo <ederson.mos@gmail.com>
2023-02-06 19:04:04 -0300

    Example import Circle, with circle depends math

---
+++ modules/circle.mjs
@@ -0,0 +1,10 @@
+import * as math from './math.mjs';
+export default class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get area() {
+        return math.PI * math.pow(this.radius, 2);
+    }
+}
---
+++ modules/example-5.mjs
@@ -0,0 +1,7 @@
+import Circle from './circle.mjs';
+const circle = new Circle(10);
+console.log(circle);
+// expect output: Circle { radius: 10 }
+
+console.log(circle.area);
+// expect output: 314.1592
