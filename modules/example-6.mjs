2a804d0e09fa0afb5a72bad6b05dd9ee01d8b08a
Ederson Macedo <ederson.mos@gmail.com>
2023-02-06 19:06:33 -0300

    Example import Circle for last assign. The name of this is hoisting

---
+++ modules/example-6.mjs
@@ -0,0 +1,8 @@
+const circle = new Circle(10);
+console.log(circle);
+// expect output: Circle { radius: 10 }
+
+console.log(circle.area);
+// expect output: 314.1592
+
+import Circle from './circle.mjs';
